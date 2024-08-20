1a74cd8c5f71dffeaa1428df8e7b7f6456c503f3
Pyrox <pyrox@pyrox.dev>
2024-08-20 19:45:42 -0400

    organizations migration regen

--- vgarchive/organizations/views.py
+++ vgarchive/organizations/views.py
@@ -2,11 +2,18 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Organization
+from vgarchive.events.models import Event
 
 
 class OrganizationDetailView(DetailView):
     model = Organization
     template_name = "organizations/detail.html"
+
+    def get_context_data(self, **kwargs):
+        # Get context
+        context = super().get_context_data(**kwargs)
+        context["events"] = Event.objects.filter(organization=self.kwargs.get("pk"))
+        return context
 
 
 class OrganizationListView(ListView):
