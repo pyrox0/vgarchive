c8c84d69b86808d024086a0ab3ff93357700b846
Pyrox <pyrox@pyrox.dev>
2024-08-22 20:50:05 -0400

    fix org list view name

--- vgarchive/organizations/urls.py
+++ vgarchive/organizations/urls.py
@@ -4,5 +4,5 @@ from .views import OrganizationDetailView, OrganizationListView
 
 urlpatterns = [
     path("<slug:pk>/", OrganizationDetailView.as_view(), name="organization-details"),
-    path("", OrganizationListView.as_view(), name="marathon-list"),
+    path("", OrganizationListView.as_view(), name="organization-list"),
 ]
