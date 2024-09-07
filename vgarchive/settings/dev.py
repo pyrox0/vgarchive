bb69f6ea0a7606baa2d1ddfc74d78fb7acdec5e1
Pyrox <pyrox@pyrox.dev>
2024-09-07 17:19:26 -0400

    update django settings files

--- vgarchive/settings/dev.py
+++ vgarchive/settings/dev.py
@@ -6,8 +6,7 @@ SECRET_KEY = "django-insecure-5pvrv(*#6@e=sk3&=zt6znv^0#9(b+5weaws_6et&fuph3*n#6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["100.125.9.36", ".localhost", "127.0.0.1", "[::1]"]
-INTERNAL_IPS = ["127.0.0.1", "100.64.0.0/10", "[::1]"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS += [
     # Hot reload
