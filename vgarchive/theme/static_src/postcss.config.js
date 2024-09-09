4fd90884841fabd453237bddf8f2a2d86614a89d
Pyrox <pyrox@pyrox.dev>
2024-09-09 13:32:22 -0400

    use postcss cli instead of tailwind cli

--- vgarchive/theme/static_src/postcss.config.js
+++ vgarchive/theme/static_src/postcss.config.js
@@ -1,7 +1,39 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
     "postcss-simple-vars": {},
-    "postcss-nested": {}
+    "postcss-nested": {},
+    cssnano: {
+      preset: "default",
+    },
+    // "@fullhuman/postcss-purgecss": {
+    //   content: [
+    //     "../templates/**/*.html",
+    //     "../../templates/**/*.html",
+    //     "../../**/templates/*.html",
+    //     "../../events/views.py",
+    //   ],
+    //   defaultExtractor: (content) => content.match(/[\w\-/:[\]]+(?<!:)/g) || [],
+    //   safelist: [
+    //     "bi",
+    //     "bi-cash-stack",
+    //     "bi-currency-dollar",
+    //     "bi-discord",
+    //     "bi-github",
+    //     "bi-globe2",
+    //     "bi-joystick",
+    //     "bi-list",
+    //     "bi-moon",
+    //     "bi-people-fill",
+    //     "bi-search",
+    //     "bi-sun",
+    //     "bi-twitch",
+    //     "bi-twitter",
+    //     "bi-youtube",
+    //     "bi-youtube",
+    //   ],
+    // },
   },
 }
