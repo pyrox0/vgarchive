d1dfb1fee4f910ac372b616b024586aa88ef2fc0
Pyrox <pyrox@pyrox.dev>
2024-09-12 01:07:14 -0400

    add charity views and related templates

--- vgarchive/charities/views.py
+++ vgarchive/charities/views.py
@@ -1,15 +1,62 @@
+import locale
+
+
 from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
+from django.utils.html import format_html
+from django.urls import reverse
+
+import django_tables2 as tables
 
 from .models import Charity
+from vgarchive.views import VGArchiveMetaTable
 
 
 class CharityDetailView(DetailView):
     model = Charity
     template_name = "charity-detail.html"
 
+    def get_context_data(self, **kwargs) -> dict:  # noqa
+        # Get context
+        context = super().get_context_data(**kwargs)
+        context["events"] = self.object.event_set.all()
 
-class CharityListView(ListView):
+        return context
+
+
+class CharityTable(tables.Table):
+    class Meta(VGArchiveMetaTable):
+        model = Charity
+        order_by = "-name"
+        exclude = ("id", "short_name", "icon")
+        sequence = (
+            "name",
+            "homepage",
+            "donation_total",
+            "founded",
+        )
+
+    name = tables.Column(verbose_name="Name")
+    homepage = tables.Column(verbose_name="Homepage", orderable=False)
+    donation_total = tables.Column(verbose_name="Donation Total", localize=True)
+    founded = tables.Column(verbose_name="Year Founded", localize=False)
+
+    def render_homepage(self, value):  # noqa
+        return format_html(
+            f'<a class="external-link link-info" href="{value}">Homepage</a>'
+        )
+
+    def render_name(self, value, record):  # noqa
+        return format_html(
+            f'<a class="text-2xl font-bold link link-primary" href="{reverse("charity-detail", args=[record.id])}">{value}</a>'
+        )
+
+    def render_donation_total(self, value):  # noqa
+        return format_html(
+            f'<p class="text-success font-bold">{locale.currency(value, True, True, False)}</p>'
+        )
+
+
+class CharityListView(tables.SingleTableView):
     model = Charity
-    paginate_by = 20
+    table_class = CharityTable
     template_name = "charity-list.html"
