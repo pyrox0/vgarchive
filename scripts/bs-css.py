62583058392225acddcdc3072cd37d9d115c774f
Pyrox <pyrox@pyrox.dev>
2024-09-13 03:35:37 -0400

    add new minified bootstrap icons css build

---
+++ scripts/bs-css.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import json
+import tinycss2
+
+classes = json.load(open("./vgarchive/theme/static_src/classes.json"))["classes"]
+
+rules = tinycss2.parse_stylesheet(
+    open("./vgarchive/theme/static_src/src/bs-icons/bootstrap-icons.css").read(),
+    skip_comments=True,
+)
+
+final_rules = rules[:4]
+
+rules = rules[4:]
+
+for i, rule in enumerate(rules):
+    if isinstance(rule, tinycss2.ast.WhitespaceToken):
+        continue
+
+    if rule.prelude[1].value not in classes:
+        del rules[i]
+    else:
+        char = hex(ord(rule.content[4].value))[2:]
+        print(rule.content[4].value)
+        rule.content[4].representation = f'"\\{char}"'
+        final_rules.append(rule)
+        final_rules.append(rules[i + 1])
+
+
+out = open("./vgarchive/theme/static_src/src/bs-icons/bootstrap-icons.min.css", "w")
+out.write(tinycss2.serialize(final_rules))
+out.close()
