8c8adde6230f51494820ab2fbad8da93b5956a7f
Pyrox <pyrox@pyrox.dev>
2024-09-14 15:55:27 -0400

    add game and runner models to vgarchive.runs

--- vgarchive/runs/admin.py
+++ vgarchive/runs/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Run
+from .models import Run, Runner, Game
 
 from vgarchive.admin import site
+
+
+@admin.register(Game, site=site)
+class GameAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Runner, site=site)
+class RunnerAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Run, site=site)
--- vgarchive/runs/models.py
+++ vgarchive/runs/models.py
@@ -1,6 +1,49 @@
+from typing import Self
 from django.db import models
 
 from vgarchive.events.models import Event
+from vgarchive.utils import is_youtube_url, is_twitch_url
+
+
+class Game(models.Model):
+    """A game that's been run at an event.
+
+    Attributes:
+        id: The game's ID
+        name: The game's name
+
+    """
+
+    name = models.CharField("Game Name", max_length=200)
+
+    def __str__(self) -> str:  # noqa
+        return self.name  # type:ignore
+
+
+class Runner(models.Model):
+    """A runner of a game.
+
+    Attributes:
+        name: Their name
+        pronouns: Their pronouns
+        twitch: Their Twitch username, can be null
+        youtube: Their Youtube channel, can be null
+        twitter: Their Twitter account username, can be null
+        bluesky: Their Bluesky account username, can be null
+    """
+
+    name = models.CharField("Name", max_length=200)
+    pronouns = models.CharField("Pronouns", max_length=50, blank=True)
+    twitch = models.CharField("Twitch Channel", max_length=25, blank=True)
+    youtube = models.CharField("Youtube Channel", max_length=200, blank=True)
+    twitter = models.CharField("Twitter Username", max_length=15, blank=True)
+    bluesky = models.CharField("Bluesky Username", max_length=200, blank=True)
+
+    def __str__(self: Self) -> str:
+        if self.pronouns:
+            return f"{self.name} ({self.pronouns})"
+
+        return f"{self.name}"
 
 
 class Run(models.Model):
@@ -23,13 +66,13 @@ class Run(models.Model):
         verbose_name="Event",
         default="none",
     )
-    game = models.CharField("Game Name", max_length=200)
+    game = models.ForeignKey(Game, models.PROTECT, verbose_name="Game")
     category = models.CharField("Category", max_length=200)
     platform = models.CharField("Platform", max_length=200)
     length = models.DurationField("Run Length")
-    runners = models.CharField("Runners", max_length=200)
-    youtube = models.URLField()
-    twitch = models.URLField()
+    runners = models.ManyToManyField(Runner, verbose_name="Runners")
+    youtube = models.URLField("Youtube VOD Link", validators=[is_youtube_url])
+    twitch = models.URLField("Twitch VOD Link", validators=[is_twitch_url])
 
     class Meta:
         db_table_comment = "Runs"
