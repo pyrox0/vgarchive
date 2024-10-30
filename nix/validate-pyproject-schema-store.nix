96099192f1ab63f60d871743f9167f5687733aca
Pyrox <pyrox@pyrox.dev>
2024-10-30 12:47:50 -0400

    validate-pyproject-schema-store: 2024.08.26 -> 2024.10.21

--- nix/validate-pyproject-schema-store.nix
+++ nix/validate-pyproject-schema-store.nix
@@ -1,14 +1,14 @@
 { python3Packages, fetchFromGitHub }:
 python3Packages.buildPythonApplication rec {
   pname = "validate-pyproject-schema-store";
-  version = "2024.08.26";
+  version = "2024.10.21";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "henryiii";
     repo = "validate-pyproject-schema-store";
     rev = version;
-    hash = "sha256-ymg0OFusy8fBV/hJXulDQwhkyFhOkr3OzhP3bd334AA=";
+    hash = "sha256-uK7UvxG7k8vT1YPheg+eyLSye2NP5pogtQ+qnK+V4w0=";
   };
 
   build-system = [ python3Packages.hatchling ];
